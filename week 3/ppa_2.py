d2263eddb83d76b1a52cfc5e077b7d52c6b08ae9
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-02-13 22:59:10 +0530

    Create ppa_2.py

---
+++ week 3/ppa_2.py
@@ -0,0 +1,9 @@
+"""
+Problem statement:
+Accept a positive integer n as input and print all the factors of n, one number on each line.
+"""
+
+n = int(input()
+for i in range(1, n+1):
+    if n%i == 0:
+        print(i)
