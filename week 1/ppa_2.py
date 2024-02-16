5e4866664e888c35991f3fb3ac389ad38876f190
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-02-16 14:24:16 +0530

    Create ppa_2.py

---
+++ week 1/ppa_2.py
@@ -0,0 +1,12 @@
+"""
+Print the following pattern.
+*
+**
+***
+****
+*****
+There are no spaces between consecutive stars. There are no spaces at the end of each line.
+"""
+
+for i in range(5):
+    print('*'*(i+1))
