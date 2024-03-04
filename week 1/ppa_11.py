2b34067ca17d066e80a366014a1eeb13ea9bb4d5
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-04 15:08:50 +0530

    Create ppa_11.py

---
+++ week 1/ppa_11.py
@@ -0,0 +1,24 @@
+"""
+Accept two integers 
+�
+a and 
+�
+b as input and print the absolute difference of both the numbers. For example, if 
+�
+=
+9
+,
+�
+=
+8
+a=9,b=8, then the absolute difference is 
+9
+−
+8
+=
+1
+9−8=1. So, your output should be 
+1
+1. You should be able to solve this problem using the concepts covered in this week.
+"""
+print(abs(int(input())-int(input())))
