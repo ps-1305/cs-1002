9750d69c62f7f7f9f2c54a865ec6c321e4020617
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-04 15:13:31 +0530

    Create grpa_5.py

---
+++ week 1/grpa_5.py
@@ -0,0 +1,4 @@
+"""
+Accept two positive integers x and y as input. Print the number of digits in xy. You should be able to solve this problem using the concepts covered in week-1.
+"""
+print(len(str(int(input())**int(input()))))
