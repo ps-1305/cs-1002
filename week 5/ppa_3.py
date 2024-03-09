cc6710331fd33f301407b6ba460f98daaf75dc85
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-09 20:36:10 +0530

    Create ppa_3.py

---
+++ week 5/ppa_3.py
@@ -0,0 +1,10 @@
+def maxval(a, b, c):
+    """
+    compute the maximum among three numbers
+
+    Arguments:
+        a, b, c: integers
+    Return:
+        max_of_three: integer
+    """
+    return max(a,b,c)
