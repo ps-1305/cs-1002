4ea81e97c1f12cbbcbff8d49d8aa7a7de8b6019c
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-13 21:08:09 +0530

    Update grpa_1.py

--- week 7/grpa_1.py
+++ week 7/grpa_1.py
@@ -1,7 +1,15 @@
-all = []
+all = dict()
+lens = []
 for _ in range(8):
     temp = list(map(str, input().split(",")))
-    all.append(temp)
+    all[temp[0]] = len(temp)-1
+    lens.append(len(temp)-1)
     temp = []
-for i in range(8):
-    print(f"{all[i][0]}:{len(all[i])-1}")
+lens = sorted(lens)
+another = dict()
+for j in lens:
+    for i in all.keys():
+        if all[i] == j:
+            another[i] = j
+for i in range(len(another)-1,-1,-1):
+    print(f"{list(another.keys())[i]}:{list(another.values())[i]}")
