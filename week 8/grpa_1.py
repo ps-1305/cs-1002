7a5a8cea17b55d08a455e55f7e24f4779a2114df
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-20 20:33:25 +0530

    Create grpa_1.py

---
+++ week 8/grpa_1.py
@@ -0,0 +1,11 @@
+def reverse(L):
+    """
+    A recursive function to reverse a list L
+
+    Arguments:
+        L: list, type of elements could be anything
+    Return:
+        result: list
+    """
+    if len(L) == 0: return []
+    return [L[-1]] + reverse(L[:-1])
