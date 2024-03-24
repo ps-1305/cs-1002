405b1049c111444244dac3173a488566f19b8517
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-25 03:10:16 +0530

    Create ppa_7.py

---
+++ week 6/ppa_7.py
@@ -0,0 +1,10 @@
+scores_dataset = []
+for i in range(int(input())):
+    temp = dict()
+    temp["Name"] = input()
+    temp["City"] = input()
+    temp["SeqNo"] = int(input())
+    temp["Mathematics"] = int(input())
+    temp["Physics"] = int(input())
+    temp["Chemistry"] = int(input())
+    scores_dataset.append(temp)
